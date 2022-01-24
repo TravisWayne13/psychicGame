c1a6970c992f0152d4493e901d79163ab698f117
Travis Houston <traviswaynehouston@gmail.com>
2022-01-24 11:47:26 -0700

    added folders and html

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+const letter = [ a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z ]
+let winningLetter = letter[Math.floor(math.random * 100)]
+// game needs to pick a random letter
+console.log(winningLetter)
+
+
+
+
+
+
+// letters need to be checked/stored for win
+
+
+
+
+
